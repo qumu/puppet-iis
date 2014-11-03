79d85253a510462784fc04e00cb3b99ba6683898
Abhishek Dastidar <abhishek.dastidar@qumu.com>
2014-11-03 13:21:44 +0000

    use powershell provider

--- manifests/manage_server.pp
+++ manifests/manage_server.pp
@@ -1,11 +1,10 @@
 define iis::manage_server($filter, $name, $value) {
-  include 'iis::param::powershell'
 
     exec { "WebConfiguration-manage-${filter}-${name}" :
-      command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; Set-WebConfigurationProperty -Filter ${filter} -Name ${name} -Value ${value} \"",
-      path      => "${iis::param::powershell::path};${::path}",
-      onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if((Get-WebConfigurationProperty -Filter ${filter} -Name ${name} ).value -eq (${value}) ) { exit 1 } else { exit 0 }\"",
+      command   => "Import-Module WebAdministration; Set-WebConfigurationProperty -Filter ${filter} -Name ${name} -Value ${value}",
+      onlyif    => "Import-Module WebAdministration; if((Get-WebConfigurationProperty -Filter ${filter} -Name ${name} ).value -eq (${value}) ) { exit 1 } else { exit 0 }",
       logoutput => true,
+      provider  => powershell
     }
 }
 
